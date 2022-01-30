8cebe8411394d191a478fbaa268d954256e92d8f
Ravi Sehrawat <ravisehrawat625@gmail.com>
2022-01-30 11:55:19 +0530

    solution of around 1

---
+++ around-1.py
@@ -0,0 +1,12 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+put("token")
+move()
+while not object_here("token"):
+    if front_is_clear():
+        move()
+    else:
+        turn_left()
