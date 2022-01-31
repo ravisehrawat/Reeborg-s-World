94ee6946aed377d9215a5adb7af56a578eb6c68b
Ravi Sehrawat <ravisehrawat625@gmail.com>
2022-01-31 12:11:18 +0530

    Another approach added

--- hurdle-1.py
+++ hurdle-1.py
@@ -1,4 +1,4 @@
-
+#solution 1
 def turn_right():
     turn_left()
     turn_left()
@@ -14,3 +14,28 @@ while not at_goal():
         turn_right()
         if front_is_clear():
             move() 
+            
+#solution 2
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+while not at_goal():
+    if front_is_clear():
+        move()
+    else:
+        jump()
+    
+
+    
