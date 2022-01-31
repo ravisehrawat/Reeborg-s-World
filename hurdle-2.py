89b970626f5025c78309923b6407fb69e2e32693
Ravi Sehrawat <ravisehrawat625@gmail.com>
2022-01-31 12:14:09 +0530

    Solution of hurdle 2

---
+++ hurdle-2.py
@@ -0,0 +1,39 @@
+#solution 1
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    if wall_on_right():
+        if front_is_clear():
+            move()
+        else:
+            turn_left()
+    else:
+        turn_right()
+        if front_is_clear():
+            move() 
+            
+#solution 2
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+while not at_goal():
+    if front_is_clear():
+        move()
+    else:
+        jump()
+    
