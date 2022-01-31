84f207891538bfc1f3312576f0213d5924279c86
Ravi Sehrawat <ravisehrawat625@gmail.com>
2022-01-31 11:28:09 +0530

    Solution of around 2

---
+++ around-2.py
@@ -0,0 +1,18 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+put("token")
+move()
+while not object_here("token"):
+    if wall_on_right():
+        if front_is_clear():
+            move()
+        else:
+            turn_left()
+    else:
+        turn_right()
+        if front_is_clear():
+            move()
+        
